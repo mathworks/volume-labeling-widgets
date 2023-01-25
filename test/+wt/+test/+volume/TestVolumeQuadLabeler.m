1a04f483192fb41a32ef750780e2c8540e56e62a
Robyn Jackey <15696076+rjackey@users.noreply.github.com>
2023-01-25 10:08:08 -0500

    replace "dog" dataset with "lung", fixes #13

--- test/+wt/+test/+volume/TestVolumeQuadLabeler.m
+++ test/+wt/+test/+volume/TestVolumeQuadLabeler.m
@@ -1,7 +1,7 @@
 classdef TestVolumeQuadLabeler < wt.test.volume.TestVolumeLabeler
     % Unit Test - Implements a unit test for a widget or component
     
-    % Copyright 2020 The MathWorks,Inc.
+    % Copyright 2020-2023 The MathWorks,Inc.
     
     
     %% Helper Methods
@@ -41,22 +41,28 @@ classdef TestVolumeQuadLabeler < wt.test.volume.TestVolumeLabeler
             
             w = testCase.createAnnotationViewer();
 
+            % Dog dataset (Removed in R2023a)
+            %dicomFolder = fullfile(matlabroot,"toolbox","images","imdata","dog");
+
+            % Lung dataset
+            zipFile = matlab.internal.examples.downloadSupportFile("medical","MedicalVolumeDICOMData.zip");
+            parentFolder = fileparts(zipFile);
+            unzip(zipFile, parentFolder)
+            dicomFolder = fullfile(parentFolder,"MedicalVolumeDICOMData","LungCT01");
+
             % Load a different dataset
-            dicomFolder = fullfile(matlabroot,"toolbox","images","imdata","dog");
             fcn = @()wt.model.VolumeModel.fromDicomFile(dicomFolder);
-            %dogModel = testCase.verifyWarning(fcn, 'VolumeModel:fromDicom:NonUniformZ', "Expected a warning");
-            %dogModel = wt.model.VolumeModel.fromDicomFile(dicomFolder);
-            dogModel = testCase.verifyWarningFree(fcn);
+            volModel = testCase.verifyWarningFree(fcn);
 
             % Assign the new dataset
-            fcn = @()set(w,"VolumeModel",dogModel);
+            fcn = @()set(w,"VolumeModel",volModel);
             testCase.verifyWarningFree(fcn);
             
             % Verify each view changed
-            testCase.verifyEqual(w.VolumeModel, dogModel)
-            testCase.verifyEqual(w.SliceView.VolumeModel, dogModel)
-            testCase.verifyEqual(w.TopView.VolumeModel, dogModel)
-            testCase.verifyEqual(w.SideView.VolumeModel, dogModel)
+            testCase.verifyEqual(w.VolumeModel, volModel)
+            testCase.verifyEqual(w.SliceView.VolumeModel, volModel)
+            testCase.verifyEqual(w.TopView.VolumeModel, volModel)
+            testCase.verifyEqual(w.SideView.VolumeModel, volModel)
   
         end %function
         
